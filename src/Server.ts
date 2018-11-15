048ac496ccdc3c4e83887dd0b0c083d98dfb5a6c
Aron Simon <aron.simon.developer@gmail.com>
2018-11-15 19:43:50 +0100

    feat(Server): check base class and decorator

--- src/Server.ts
+++ src/Server.ts
@@ -1,12 +1,45 @@
 const HapiJS = require('hapi');
+import * as HapiTypes from 'hapi';
 
-export function Server(config: { port: number, host: string }) {
-    return function (target: any) {
-        let server = HapiJS.server({
+export class ServerBase {
+    public static server: any;
+    constructor() {
+        console.log(ServerBase.server);
+    }
+    public static start () {
+        if (ServerBase.server == undefined) {
+            throw new Error(`Class ${this.name} must be decorated with @Server!`);
+        }
+    }
+}
+
+function checkBaseTypes(target: any) {
+    let temp = target.prototype;
+        let hasServerBaseType = false;
+        while (temp.constructor.name != 'Object') {
+            if(temp.constructor.name == 'ServerBase') {
+                hasServerBaseType = true;
+                break;
+            }
+            temp = temp.__proto__;
+        }
+        if (hasServerBaseType == false) throw new Error(`Class ${target.name} must extends from ServerBase!`);
+}
+
+export function Server(config: { port: number, host: string, controllers?: object[] }) {
+    return function <T extends {new(...arg: any[]): ServerBase}>(target: T) {
+        checkBaseTypes(target);
+
+        let newClass = class C extends target {
+            public static server: HapiTypes.Server;
+            public static start() {
+                C.server.start();
+            }
+        };
+        newClass.server = HapiJS.server({
             port: config.port,
             host: config.host
         })
-
-        server.start();
+        return newClass;
     }
 }
--- tests/index.ts
+++ tests/index.ts
@@ -1,19 +1,2 @@
-import * as assert from 'assert';
-import * as request from 'request';
-import { Server } from '../src';
-
-@Server({
-    port: 3000,
-    host: 'localhost'
-})
-class TestServer { }
-
-describe('@Server', () => {
-    it('should start a HapiJS server on localhost:3000', (done) => {
-        request.get('http://localhost:3000/', (err, res, body) => {
-            assert.equal(err, undefined);
-            assert.equal(res.statusCode, 404);
-            done();
-        })
-    })
-})
+require('./ServerTest');
+require('./ControllerTest');
